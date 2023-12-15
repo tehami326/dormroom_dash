36ffb85ced1acccf710eac257938d177aa6be3aa
Rishav Das <rdofficial192@gmail.com>
2023-12-15 22:49:00 +0530

    Adding all the required backend with serving the home page + redirection towards the rooms page

---
+++ static/js/index.js
@@ -0,0 +1,44 @@
+/*
+index.js
+
+The vanillaJs for the frontend responsiveness of the index page of the web app.
+*/
+
+// Adding the onclick event listener to the contact us btn
+try {
+	document.getElementById("send-contact-req-btn").addEventListener("click", (event) => {
+		event.preventDefault();
+
+		// Getting the user entered data
+		const customer_msg = document.getElementById("send-contact-req-msg").value;
+		const customer_contact = document.getElementById("send-contact-req-contact").value;
+
+		// Validating the user input
+		if ((customer_msg.length > 49) && (customer_msg.length <= 500)) {
+			if (/^[0-9]{9,11}/.test(customer_contact)) {
+				// If the user input validates
+				// Sending the POST requests
+				fetch("/", {
+					method: "POST",
+					body: JSON.stringify({
+						task: "contact request",
+						customer_msg: customer_msg,
+						customer_contact: customer_contact,
+					}),
+					headers: {"Content-type": "application/json; charset=UTF-8"},
+				}).then(response => response.text()).then(response => {
+					alert(response);
+				}).catch(error => {
+					alert(error);
+				});
+			} else {
+				alert("Please enter an appropriate phone number!");
+			}
+		} else {
+			alert("Please enter an appropriate message longer than 50 characters and shorter than 500.");
+		}
+	});
+} catch (error) {
+	// If there occurs an error
+	console.log(error);
+}
